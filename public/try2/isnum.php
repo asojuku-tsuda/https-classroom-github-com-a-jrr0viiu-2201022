17035b9dff77cbcd7ed7e182ba2d69105c1f7647
2201022 <133575534+2201022@users.noreply.github.com>
2025-04-15 03:17:09 +0000

    try2andtry3

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -9,9 +9,19 @@
     <div class="login-box">
       <h2>
 <?php
+//入力欄の配列チェック
+$indata = filter_input(INPUT_GET,'indata');
 
+//入力欄に値が入っているか
+if(isset($indata) == false){
+  die("数字を入力してください。");
+}
 
-echo "入力された数字は： " . $_GET['indata'];
+//正規表現での文字チェック
+if(mb_ereg('^[0-9]+$',$indata) == false){
+  die("数字を入力してください。");
+}
+echo "入力された数字は： " . $indata;
 ?>
     </h2>
     </div>
--- public/try3/inputuser.php
+++ public/try3/inputuser.php
@@ -9,10 +9,25 @@
     <div class="login-box">
       <h2>
 <?php
+$username = filter_input(INPUT_GET,'username');
+$useraddress = filter_input(INPUT_GET,'useraddress');
+$usermail = filter_input(INPUT_GET,'usermail');
+if(mb_ereg('^[ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠ー]+$',$username) == false || mb_strlen($username) > 20){
+  die("20文字以内で名前を入力してください。記号等は利用できません。");
+}
+
+if(mb_ereg('^[ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠0-9０-９ ー]+$',$useraddress) == false || mb_strlen($useraddress) > 30){
+  die("30文字以内で住所を入力してください。記号等は利用できません。");
+}
+
+if(mb_ereg('^[a-zA-Z0-9._@-]+$',$usermail) == false){
+  die("正しいメールアドレス形式で入力してください。記号は.-_@のみ利用可能。");
+}
+
 echo "あなたが入力した値<br>";
-echo "名前：" . $_GET['username'] . "<br>";
-echo "住所：" . $_GET['useraddress']. "<br>";
-echo "メールアドレス：" . $_GET['usermail'];
+echo "名前：" . $username. "<br>";
+echo "住所：" . $useraddress. "<br>";
+echo "メールアドレス：" . $usermail;
 ?>
     </h2>
     </div>
